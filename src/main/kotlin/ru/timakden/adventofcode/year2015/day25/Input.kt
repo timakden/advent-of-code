86ca28e1bb2acbbaaa9f96bbbf9e9927294a1888
Denis Timakov <timakden88@gmail.com>
2017-03-08 01:51:49 +0300

    Added solution for year 2015, day 25

---
+++ src/main/kotlin/ru/timakden/adventofcode/year2015/day25/Input.kt
@@ -0,0 +1,3 @@
+package ru.timakden.adventofcode.year2015.day25
+
+val input = Pair(3010, 3019)
---
+++ src/main/kotlin/ru/timakden/adventofcode/year2015/day25/Puzzle.kt
@@ -0,0 +1,19 @@
+package ru.timakden.adventofcode.year2015.day25
+
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) {
+    val elapsedTime = measureTimeMillis {
+        println("Part One: ${solve(input)}")
+    }
+    println("Elapsed time: $elapsedTime ms")
+}
+
+fun solve(input: Pair<Int, Int>): Long {
+    val iterations = (1..input.second).sum() + 0.until(input.first - 1).sumBy { input.second + it }
+    var code = 20151125L
+    1.until(iterations).forEach { code = generateNextCode(code) }
+    return code
+}
+
+fun generateNextCode(previousCode: Long) = (previousCode * 252533) % 33554393
