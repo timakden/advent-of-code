4a7a91ef3beb02f8b951f86b4a778997dd65ad25
Denis Timakov <timakden88@gmail.com>
2021-05-28 10:00:26 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     idea
-    id("com.github.ben-manes.versions") version "0.38.0"
-    kotlin("jvm") version "1.5.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
+    kotlin("jvm") version "1.5.10"
 }
 
 group = "ru.timakden"
@@ -19,7 +19,7 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.code.gson:gson:2.8.7")
     implementation("javax.xml.bind:jaxb-api:2.3.1")
 
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
