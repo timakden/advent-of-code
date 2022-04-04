cdd224ba1ec2c479d4fccdf5030be6f8b3507b8e
Denis Timakov <timakden88@gmail.com>
2022-04-04 20:17:55 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.42.0"
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.20"
 }
 
 group = "ru.timakden"
@@ -13,7 +13,7 @@ repositories {
     mavenCentral()
 }
 
-val kotestVersion = "5.1.0"
+val kotestVersion = "5.2.2"
 
 dependencies {
     implementation(kotlin("reflect"))
