190beda699e5015dc96b2130cc532d2ee511c1f1
Denis Timakov <timakden88@gmail.com>
2022-12-13 20:50:44 +0300

    Added solution for year 2022, day 13

---
+++ src/main/kotlin/ru/timakden/aoc/year2022/Day13.kt
@@ -0,0 +1,93 @@
+package ru.timakden.aoc.year2022
+
+import ru.timakden.aoc.util.measure
+import ru.timakden.aoc.util.readInput
+import kotlin.time.ExperimentalTime
+
+object Day13 {
+    @JvmStatic
+    @ExperimentalTime
+    fun main(args: Array<String>) {
+        measure {
+            val input = readInput("year2022/Day13")
+            println("Part One: ${part1(input)}")
+            println("Part Two: ${part2(input)}")
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        var pairIndex = 0
+        return (0..input.lastIndex step 3).map { i ->
+            val list1 = buildList(input[i])
+            val list2 = buildList(input[i + 1])
+            pairIndex++
+            pairIndex to compare(list1, list2)
+        }.filter { it.second == -1 }.sumOf { it.first }
+    }
+
+    fun part2(input: List<String>) = input.filter { it.isNotBlank() }
+        .map { buildList(it) }
+        .toMutableList()
+        .also {
+            it.add(buildList("[[2]]"))
+            it.add(buildList("[[6]]"))
+        }.sortedWith { o1, o2 -> compare(o1, o2) }
+        .mapIndexed { index, anies -> index + 1 to anies }
+        .filter { it.second == listOf(listOf(2)) || it.second == listOf(listOf(6)) }
+        .fold(1) { acc, i -> acc * i.first }
+
+    private fun buildList(input: String): List<Any> {
+        val stack = ArrayDeque<MutableList<Any>>()
+        var currentList: MutableList<Any>? = null
+        var string = input
+
+        while (string.isNotEmpty()) {
+            when {
+                string.first() == '[' -> {
+                    currentList?.let { stack.addLast(it) }
+                    currentList = mutableListOf()
+                    string = string.drop(1)
+                }
+
+                string.first() == ']' -> {
+                    stack.removeLastOrNull()?.let { previousList ->
+                        previousList.add(currentList as Any)
+                        currentList = previousList
+                    }
+                    string = string.drop(1)
+                }
+
+                string.first() == ',' -> string = string.drop(1)
+
+                else -> {
+                    "\\d+".toRegex().find(string)?.value?.toIntOrNull()?.let {
+                        currentList?.add(it)
+                        string = if (it > 9) string.drop(2) else string.drop(1)
+                    }
+                }
+            }
+        }
+
+        return currentList!!
+    }
+
+    private fun compare(left: Any, right: Any): Int {
+        if (left is Int && right is Int) return left.compareTo(right)
+
+        val leftIterator = (if (left is List<*>) left else listOf(left)).iterator()
+        val rightIterator = (if (right is List<*>) right else listOf(right)).iterator()
+
+        while (true) {
+            val hasLeft = leftIterator.hasNext()
+            val hasRight = rightIterator.hasNext()
+
+            if (!hasLeft && !hasRight) return 0
+            else if (!hasLeft) return -1
+            else if (!hasRight) return 1
+            else {
+                val result = compare(checkNotNull(leftIterator.next()), checkNotNull(rightIterator.next()))
+                if (result != 0) return result
+            }
+        }
+    }
+}
--- src/test/kotlin/ru/timakden/aoc/AdventOfCodeSpec.kt
+++ src/test/kotlin/ru/timakden/aoc/AdventOfCodeSpec.kt
@@ -975,6 +975,42 @@ class AdventOfCodeSpec : ShouldSpec({
                 ru.timakden.aoc.year2022.Day12.part2(input) shouldBe expected
             }
         }
+
+        context("Day 13: Distress Signal") {
+            val input = listOf(
+                "[1,1,3,1,1]",
+                "[1,1,5,1,1]",
+                "",
+                "[[1],[2,3,4]]",
+                "[[1],4]",
+                "",
+                "[9]",
+                "[[8,7,6]]",
+                "",
+                "[[4,4],4,4]",
+                "[[4,4],4,4,4]",
+                "",
+                "[7,7,7,7]",
+                "[7,7,7]",
+                "",
+                "[]",
+                "[3]",
+                "",
+                "[[[]]]",
+                "[[]]",
+                "",
+                "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+                "[1,[2,[3,[4,[5,6,0]]]],8,9]"
+            )
+            should("Part One") {
+                val expected = 13
+                ru.timakden.aoc.year2022.Day13.part1(input) shouldBe expected
+            }
+            should("Part Two") {
+                val expected = 140
+                ru.timakden.aoc.year2022.Day13.part2(input) shouldBe expected
+            }
+        }
     }
 
     context("Utils") {
