248e866bde7d759ee5cfaab9b7783d020827a25e
Denis Timakov <timakden88@gmail.com>
2023-03-21 08:26:59 +0300

    Added solution for year 2022, day 22

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     idea
-    id("com.github.ben-manes.versions") version "0.45.0"
+    id("com.github.ben-manes.versions") version "0.46.0"
     kotlin("jvm") version "1.8.10"
     kotlin("plugin.serialization") version "1.8.10"
 }
@@ -20,7 +20,7 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("io.arrow-kt:arrow-core:1.1.5")
 
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
