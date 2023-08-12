76aa46cabd02617e9c49e8977e94539857fc29ce
Denis Timakov <timakden88@gmail.com>
2023-08-12 21:30:48 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,9 @@ val kotestVersion = "5.6.2"
 dependencies {
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("io.arrow-kt:arrow-core:1.2.0-RC")
+    implementation("io.arrow-kt:arrow-core:1.2.0")
 
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
