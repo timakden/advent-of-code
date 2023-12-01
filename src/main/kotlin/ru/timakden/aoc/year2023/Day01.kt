e854fa3785ff0f8f1120964b33013c3372674106
Denis Timakov <timakden88@gmail.com>
2023-12-01 16:31:34 +0300

    Updated solution for year 2023, day 1

--- src/main/kotlin/ru/timakden/aoc/year2023/Day01.kt
+++ src/main/kotlin/ru/timakden/aoc/year2023/Day01.kt
@@ -14,36 +14,52 @@ object Day01 {
     }
 
     fun part1(input: List<String>): Int {
-        val digits = ('0'..'9').toList()
-        return input.sumOf { s ->
-            val first = s.first { it in digits }
-            val second = s.last { it in digits }
+        val digits = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
+        return input.sumOf {
+            val first = it.findAnyOf(digits)?.second
+            val second = it.findLastAnyOf(digits)?.second
             "$first$second".toInt()
         }
     }
 
     fun part2(input: List<String>): Int {
-        return input.map { s ->
-            val regex = "(?=(1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine))".toRegex()
-            val found = regex.findAll(s).toList().map { it.groupValues }.map { it.last() }
-            found.joinToString("") {
-                when (it) {
-                    "one" -> "1"
-                    "two" -> "2"
-                    "three" -> "3"
-                    "four" -> "4"
-                    "five" -> "5"
-                    "six" -> "6"
-                    "seven" -> "7"
-                    "eight" -> "8"
-                    "nine" -> "9"
-                    else -> it
-                }
-            }
-        }.sumOf { s ->
-            val first = s.first()
-            val second = s.last()
+        val digits = listOf(
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+        )
+        return input.sumOf {
+            val first = it.findAnyOf(digits)?.second?.parseDigit()
+            val second = it.findLastAnyOf(digits)?.second?.parseDigit()
             "$first$second".toInt()
         }
+    }
+
+    private fun String.parseDigit() = when (this) {
+        "one" -> "1"
+        "two" -> "2"
+        "three" -> "3"
+        "four" -> "4"
+        "five" -> "5"
+        "six" -> "6"
+        "seven" -> "7"
+        "eight" -> "8"
+        "nine" -> "9"
+        else -> this
     }
 }
