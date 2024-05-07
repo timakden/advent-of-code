763d83062626edd672f775b15208ed6a39fa3678
Denis Timakov <timakden88@gmail.com>
2024-05-07 14:14:20 +0300

    Update arrow and kotlin versions
    The Kotlin and Arrow versions have been updated in both the Gradle properties and build file to improve the functionality and performance of the project. Arrow has been updated from 1.2.3 to 1.2.4 and Kotlin from 1.9.23 to 1.9.24.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ val serializationVersion: String by project
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.serialization") version "1.9.23"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.serialization") version "1.9.24"
 }
 
 group = "ru.timakden"
