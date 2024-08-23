8cd88cd91bb88901f1086edc19e8047606e236be
Denis Timakov <timakden88@gmail.com>
2024-08-23 10:55:17 +0300

    Update Gradle configuration and dependencies
    Replaced wrapper validation with setup action in workflow. Updated Gradle version to 8.10 and Kotlin plugin to 2.0.20 for better compatibility and performance. Simplified workflow by directly running tests with Gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("jvm") version "2.0.10"
-    kotlin("plugin.serialization") version "2.0.10"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 group = "ru.timakden"
@@ -43,7 +43,7 @@ tasks {
         useJUnitPlatform()
     }
     wrapper {
-        gradleVersion = "8.9"
+        gradleVersion = "8.10"
     }
 }
 
